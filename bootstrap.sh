6644f0c372ad8f03c34d4c7030534226ca8e77a8
Bob Rohan <bob.rohan@bob-rohan.lan>
2020-12-14 15:54:53 +0000

    fair bit of change, lost track

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,6 @@
 
 __script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# TODO bugfix, existing crontab contents not preserved
-echo "installing crontab ${__script_dir}/.crontab"
-crontab -l 2>/dev/null; cat ${__script_dir}/.crontab | crontab -
-
 FILENAMES=( .functions .aliases .zshrc )
 for FILENAME in "${FILENAMES[@]}"; do
   echo "installing ${__script_dir}/$FILENAME"
